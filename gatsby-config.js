f8eb7ad158ff3482254646df7ed9352e99f2b4db
gixxerblade <sdclarkie@gmail.com>
2020-03-30 17:26:16 -0400

    prepended api env keys with GATSBY_ also gatsbyconfig.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-source-stripe`,
       options: {
         objects: ["Product", "Sku", "Order"],
-        secretKey: process.env.STRIPE_SECRET_KEY,
+        secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
         downloadFiles: true,
         auth: false
       }
