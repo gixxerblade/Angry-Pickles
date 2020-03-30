557b598f4044f90b82884e9d3d2101cf780e4c3c
gixxerblade <sdclarkie@gmail.com>
2020-03-30 19:55:57 -0400

    added navigate to /home if order summary fails fetch

--- src/components/Fetcher.js
+++ src/components/Fetcher.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { navigate } from "gatsby";
 export const useFetch = (url, options) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -10,14 +10,14 @@ export const useFetch = (url, options) => {
       const data = await response.json();
       const type = await response.ok;
       const { data: order } = data;
-      if (isSubscribed && type===true) {
+      if (isSubscribed && type === true) {
         setData(order);
         setLoading(false);
         console.log("JSON: ", type);
         console.log("Fetch Data: ", order);
-      }
-      else{
-        throw new Error("HTTP status " + response.status);
+      } else {
+        console.error("HTTP status " + response.status);
+        navigate("/home");
       }
     };
     fetchData();
