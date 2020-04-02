3a6307d1d327c8332d9246be43dee8bc412c4cae
gixxerblade <sdclarkie@gmail.com>
2020-04-02 14:02:14 -0400

    retrieve function troubleshooting

--- src/functions/retrieve.js
+++ src/functions/retrieve.js
@@ -1,40 +1,42 @@
 const stripe = require("stripe")(process.env.GATSBY_STRIPE_SECRET_KEY);
 
+const errorResponse = (err, callback) => {
+  const response = {
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    statusCode: 500,
+    body: JSON.stringify({
+      error: err
+    })
+  };
+
+  if (typeof callback === "function") {
+    callback(null, response);
+  }
+};
+
 /*
  * Retrieve an order by order id.
  */
 module.exports.handler = async (event, context, callback) => {
   const query = event.queryStringParameters.id;
-  /*   
-const id = query.id;
- */
-  return stripe.orders.retrieve(`${query}`, (err, order) => {
-    let statusCode, body;
-    if (err) {
-      statusCode = !200;
-      body = JSON.stringify({
-        error: err.message
-      });
-    } else {
-      statusCode = 200;
-      body = JSON.stringify({
-        data: order
-      });
-    }
+  try {
+    const order = await stripe.orders.retrieve(query);
     const response = {
       headers: {
         "Access-Control-Allow-Origin": "*"
       },
-      statusCode,
-      body
+      statusCode: 200,
+      body: JSON.stringify({
+        data: order,
+        message: "Order placed successfully!"
+      })
     };
     console.log("Type ID: ", typeof query);
     console.log("ID: ", `${query}`);
-    console.log(
-      "event.queryStringParameters.id: ",
-      event.queryStringParameters.id
-    );
-    console.log("Error: ", err);
-     callback(null, response);
-  });
+    callback(null, response);
+  } catch (e) {
+    errorResponse(e, callback);
+  }
 };
