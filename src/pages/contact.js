652cee549b15008640fbc1243b198a6a0707200c
gixxerblade <sdclarkie@gmail.com>
2020-04-13 15:47:29 -0400

    Fix syntax error. Removed prefix GATSBY_ in env variable on recaptcha const

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { navigate } from "gatsby";
 import Recaptcha from "react-google-recaptcha";
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY;
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 if (typeof RECAPTCHA_KEY === "undefined") {
   throw new Error(`
   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
