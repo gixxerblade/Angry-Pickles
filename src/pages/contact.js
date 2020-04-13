c629572b69027541293aa6438b5f0bbe6bf25f33
gixxerblade <sdclarkie@gmail.com>
2020-04-13 15:56:53 -0400

    syntax error in contacts

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,7 @@ import Recaptcha from "react-google-recaptcha";
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 if (typeof RECAPTCHA_KEY === "undefined") {
-  throw new Error(`
-  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
+  throw new Error(`Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
   You probably forget to set it in your Netlify build environment variables. 
   Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings`);
 }
